d47ed9b8e326febe6ca15f694354cd6b3df184e1
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2023-02-01 09:13:49 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -6,29 +6,29 @@ untuk lainya saran jangan
 di ubah bre nanti eror]]*/
 
 /*global owner*/
-global.owner = ['6281395861695']  
-global.mods = ['6281395861695'] 
-global.prems = ['6281395861695']
+global.owner = ['6285878836361']  
+global.mods = ['6285878836361'] 
+global.prems = ['6285878836361']
 /*Nomor Owner & payment*/
-global.nameowner = 'Tio'
-global.numberowner = '6281395861695' 
+global.nameowner = 'rizki'
+global.numberowner = '6285878836361' 
 global.mail = 'tioclkp02@gmail.com' //enter your email 🗿
-global.dana = '6281395861695'
-global.pulsa = '6281395861695'
-global.gopay = '6281395861695'
+global.dana = 'chah owner'
+global.pulsa = 'chatowner'
+global.gopay = 'chatowner'
 /*Nama Bot & Grouplink*/
-global.namebot = 'RTXZY-MD'
-global.gc = 'https://chat.whatsapp.com/D6BZVRSymWaFU28t2lxaO0'
-global.web = 'https://github.com/BOTCAHX'
-global.instagram = 'https://instagram.com/prm2.0'
+global.namebot = 'ikyboy-MD'
+global.gc = 'https://chat.whatsapp.com/KcMIbBUddqjIoNmGCrRU5m'
+global.web = 'https://github.com/rizkiwibu'
+global.instagram = 'https://instagram.com/ikykunnnn'
 /*global wm*/
 global.lolkey = 'rey2k22'
 global.zenzkey = 'BagasPrdn'
-global.wm = '© R-TXZY-MD'
+global.wm = '© ikybot-MD'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
-global.wm4 = '© BOT RTXZY'
+global.wm2 = '⫹⫺ Ikybot6285878836361'
+global.wm3 = '© Ikybot6285878836361'
+global.wm4 = '© Ikybot6285878836361'
 global.fla = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text='
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
@@ -36,7 +36,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
 global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.author = 'Bot WhatsApp\nIkybot6285878836361'
 /*Rest api setting*/
 global.APIs = { // API Prefix
   // name: 'https://website'
